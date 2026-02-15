d524150f9abf4a905c6e42b984e037a4f5855e42
HamzaShaikh515 <shaikhhamza515786@gmail.com>
2026-02-15 21:05:32 +0530

    Added .gitignore, added middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from app.engine import analyze_area
@@ -9,6 +10,14 @@ from geoalchemy2.shape import from_shape
 from shapely.geometry import Point
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class AreaRequest(BaseModel):
